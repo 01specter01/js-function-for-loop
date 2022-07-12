e3c87dce93db5032fb4f9f9c987bae35266dd4e9
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-12 12:21:38 +0200

    lets go

---
+++ solution.js
@@ -0,0 +1,59 @@
+// Wir haben die folgende Funktion erhalten, die wir für zwei Aufträge verwenden wollen.
+// Die Funktion selbst dürfen wir nicht verändern, aber beliebig oft ausführen.
+
+// Wichtig:
+// Unsere zwei Aufträge sollen mit beliebig vielen Zufallszahlen funktionieren.
+// Wir müssen daher darauf achten, dass unser Algorithmus mit unterschiedlichen Mengen arbeiten kann.
+
+function getRandomNumbers(amount) {
+    const numbers = Array();
+
+    for (let i = 0; i < amount; i++) {
+        numbers.push(parseInt(Math.random() * 100));
+    }
+
+    return numbers;
+}
+
+// Beispiel: gib 10 Zufallszahlen aus und speichere sie in der Variable randomNumbers
+const randomNumbers = getRandomNumbers(10);
+console.log(randomNumbers);
+
+// --------------------------------------------------------------------------
+
+// 1. Gib die Anzahl der Zahlen unter 50 aus.
+// Gib zur Kontrolle auch das Array der Zufallszahlen aus.
+
+// Beispiel:
+// getRandomNumbers(5) => [1, 59, 40, 50, 99]
+// Ausgabe:
+// Es gibt 2 Zahlen unter 50.
+
+// Teste deinen Algorithmus mit den Anzahlen 10, 20, 100 und beliebig vielen weiteren.
+
+// if (amount.Array() < 50) {
+//     console.log(`One value ${randomNumber} `);
+// } else {
+//     console.log(`over 50 ${randomNumber}`);
+// }
+let counter = 0;
+for (let i = 0; i < randomNumbers.length; i++) {
+    if (randomNumbers[i] < 50) {
+        counter++;
+    }
+}
+console.log(counter);
+
+// if(randomNumbers < 50 === true)
+// --------------------------------------------------------------------------
+
+// 2. Gib zwei Arrays aus: Eines mit den Zahlen unter 50, und eines mit den Zahlen größer oder gleich 50.
+// Gib zur Kontrolle auch das Array der Zufallszahlen aus.
+
+// Beispiel:
+// getRandomNumbers(5) => [1, 59, 40, 50, 99]
+// Ausgabe:
+// Zahlen unter 50: [1, 40]
+// Zahlen größer oder gleich 50: [59, 50, 99]
+
+// Teste deinen Algorithmus mit den Anzahlen 10, 20, 100 und beliebig vielen weiteren.
